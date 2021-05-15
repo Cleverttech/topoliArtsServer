12bab0288d2e4d26ff49f8868b2589ce3a64ab46
Clever Tedeku <clevertedeku@gmail.com>
2021-05-15 21:48:24 +0200

    handle create portfolio logic

--- models/Portfolio.model.js
+++ models/Portfolio.model.js
@@ -2,10 +2,6 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const portfolioSchema = new Schema({
-  // cover: {
-  //   type:String,
-  //   required: true,
-  // },
   title: {
     type: String,
     required: true,
@@ -14,7 +10,11 @@ const portfolioSchema = new Schema({
     type: String,
     required: true,
   },
-  // images: {type: [String]},
+  cover: {
+    type: String,
+    required: true,
+  },
+  images: { type: [String] },
 });
 
 const Portfolio = model("Portfolio", portfolioSchema);
--- routes/portfolio.routes.js
+++ routes/portfolio.routes.js
@@ -16,10 +16,10 @@ const isLoggedIn = (req, res, next) => {
 
 // isLoggedIn
 router.post("/portfolio/create", isLoggedIn, (req, res) => {
-  const { title, description } = req.body;
+  const { title, description, cover, image1 } = req.body;
   const mentor = req.session.loggedInUser._id;
 
-  PortfolioModel.create({ mentor, title, description })
+  PortfolioModel.create({ mentor, title, description, cover, images: image1 })
     .then((response) => {
       res.status(200).json(response);
     })
@@ -30,59 +30,5 @@ router.post("/portfolio/create", isLoggedIn, (req, res) => {
       });
     });
 });
-
-// //check this before
-// router.get('/courses/:courseId', isLoggedIn,(req, res) => {
-//   CoursesModel.findById()
-//   .then((response) => {
-//     res.status(200).json(response)
-//   })
-//   .catch((err) => {
-//     res.status(500).json({
-//       error: 'Something went wrong',
-//       message: err
-//     })
-//   })
-// })
-
-// //used for payment
-// router.post('/courses/:courseId', isLoggedIn,(req, res) => {
-//   CoursesModel.findById()
-//   .then((response) => {
-//     res.status(200).json(response)
-//   })
-//   .catch((err) => {
-//     res.status(500).json({
-//       error: 'Something went wrong',
-//       message: err
-//     })
-//   })
-// })
-
-// router.delete('/courses/:courseId', isLoggedIn,(req, res) => {
-//   TodoModel.findByIdAndDelete(req.params.courseId)
-//   .then((response) => {
-//     res.status(200).json(response)
-//   })
-//   .catch((err) => {
-//     res.status(500).json({
-//       error: 'Something went wrong',
-//       message: err
-//     })
-//   })
-// })
-
-// router.get("portfolio", (req, res, next) => {
-//   CoursesModel.find()
-//   .populate('mentor')
-//   .then((courses) => {
-//     res.status(200).json(courses)
-//   }).catch((err) => {
-//     res.status(500).json({
-//       error: 'No courses found',
-//       message: err
-//     })
-//   });
-// });
 
 module.exports = router;
