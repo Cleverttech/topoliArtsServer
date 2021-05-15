91f56f562f4fd91997d46379170930cc205302c8
Clever Tedeku <clevertedeku@gmail.com>
2021-05-15 16:43:39 +0200

    pull updates

--- routes/artists.routes.js
+++ routes/artists.routes.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const UserModel = require('../models/User.model')
-
+const UserModel = require("../models/User.model");
 
 router.get("/artists", (req, res, next) => {
   UserModel.find()
-  .populate('courses')
-  .populate('portfolio')
-  .then((result) => {
-    // FILTER
-      res.status(200).json(result)
-    
-  }).catch((err) => {
-    res.status(500).json({
-      error: 'No Artists found',
-      message: err
+    .populate("courses")
+    .populate("portfolio")
+    .then((users) => {
+      // FILTER user roles--to return main & admin roles
+      res.status(200).json(result);
     })
-  });
+    .catch((err) => {
+      res.status(500).json({
+        error: "No Artists found",
+        message: err,
+      });
+    });
 });
 
 module.exports = router;
