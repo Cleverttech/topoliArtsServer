8ace3960b1e457b4e13de3819e5057ff402719c3
Clever Tedeku <clevertedeku@gmail.com>
2021-05-17 23:19:03 +0200

    save changes

--- routes/artists.routes.js
+++ routes/artists.routes.js
@@ -5,10 +5,25 @@ const UserModel = require("../models/User.model");
 
 router.get("/artists", (req, res, next) => {
   UserModel.find()
-    .populate("courses")
     .populate("portfolio")
-    .then((users) => {
+    .then((result) => {
       // FILTER user roles--to return main & admin roles
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "No Artists found",
+        message: err,
+      });
+    });
+});
+router.get("/artists/:artistId", (req, res, next) => {
+  const { artistId } = req.params;
+
+  UserModel.findById(artistId)
+    .populate("portfolio")
+    .then((result) => {
+      console.log("results freaking data");
       res.status(200).json(result);
     })
     .catch((err) => {
