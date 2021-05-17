8c207be6413548d7b5a8e2814d09782363274195
Clever Tedeku <clevertedeku@gmail.com>
2021-05-17 17:12:44 +0200

    implement multiple files uploads

--- routes/file-upload.routes.js
+++ routes/file-upload.routes.js
@@ -1,18 +1,43 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 // include CLOUDINARY:
-const uploader = require('../config/cloudinary.config.js');
+const uploader = require("../config/cloudinary.config.js");
 
+const customMiddle = (req, res, next) => {
+  console.log(req.body);
+  console.log(req);
+  // if (req.body == "singleImage") {
+  //   uploader.single("imageUrl");
+  // } else {
+  //   uploader.multiple("imageUrl");
+  // }
+};
 
-router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
-    if (!req.file) {
-      next(new Error('No file uploaded!'));
-      return;
-    }
-    res.status(200).json({
-      image : req.file.path
-    })
-})
+router.post("/uploadmultiple", uploader.array("imageUrl"), (req, res, next) => {
+  console.log("Here manish", req.files);
+
+  if (!req.files) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  let paths = req.files.map((el) => {
+    return el.path;
+  });
+
+  res.status(200).json({
+    images: paths,
+  });
+});
+router.post("/upload", uploader.single("imageUrl"), (req, res, next) => {
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  res.status(200).json({
+    image: req.file.path,
+  });
+});
 
 module.exports = router;
