6ad4e0bd2339f3737ae11f9fb384e79cb907ed20
Clever Tedeku <clevertedeku@gmail.com>
2021-05-21 11:43:27 +0200

    upload minified assets

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
     },
+
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/topoliArts",
       ttl: 24 * 60 * 60,
